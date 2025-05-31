13793a50afed163ecd77dd402747ac3742e751bf
Axel Closas <axelclosas13@gmail.com>
2025-05-31 05:04:12 -0300

    Implementar controladores para las rutas de superhéroes en el servidor

--- server.mjs
+++ server.mjs
@@ -1,4 +1,5 @@
 import express from 'express'
+import { buscarSuperheroesPorAtributoController, obtenerSuperheroePorIdController, obtenerSuperheroesMayoresDe30Controller } from './controllers/superheroesController.mjs'
 
 // Se inicializa la app de Express
 const app = express()
@@ -11,15 +12,15 @@ app.get('/', (req, res) => res.send('Ruta raíz'))
 
   // Recibe un ID de Superhéroe y devuelve los datos de ese superhéroe o un mensaje si no fue encontrado
   // Solicitud: /superheroes/id/:id
-app.get('/superheroes/id/:id', (req, res) => res.send('De momento nada...'))
+app.get('/superheroes/id/:id', obtenerSuperheroePorIdController)
 
   // Recibe un atributo (por ejemplo, nombre o poder) y devuelve una lista de superheroes que cumplan con ese criterio
   // Solicitud: /superheroes/atributo/:atributo/:valor
-app.get('/superheroes/atributo/:atributo/:valor', (req, res) => res.send('De momento nada... x2'))
+app.get('/superheroes/atributo/:atributo/:valor', buscarSuperheroesPorAtributoController)
 
   // Devuelve una lista de superhéroes mayores a 30 años que además sean del planeta Tierra y tengan al menos 2 poderes
   // Solicitud: /superheroes/edad/mayorA30
-app.get('/superheroes/edad/mayorA30', (req, res) => res.send('De momento nada... x3'))
+app.get('/superheroes/edad/mayorA30', obtenerSuperheroesMayoresDe30Controller)
 
 
 
