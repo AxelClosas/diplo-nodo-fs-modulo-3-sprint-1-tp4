874dbdeaf98cdadcb3579499c9cd7a2486fcbdff
Axel Closas <axelclosas13@gmail.com>
2025-06-02 10:36:05 -0300

    Refactor de funciones que retornan los datos solicitados

--- controllers/superheroesController.mjs
+++ controllers/superheroesController.mjs
@@ -4,24 +4,25 @@ import { renderizarListaSuperheroes, renderizarSuperheroe } from "../views/respo
 export function obtenerSuperheroePorIdController(req, res) {
   const { id } = req.params
   const superheroe = obtenerSuperheroePorId(parseInt(id))
-  if (superheroe){
-    res.send(renderizarSuperheroe(superheroe))
-  } else {
+  if (!superheroe) // Si es undefined, entra en el IF y retorna el mensaje de error
     res.status(404).send({mensaje: 'Superhéroe no encontrado.'})
-  }
+
+  res.status(200).send(renderizarSuperheroe(superheroe)) // Modificación a partir de este video: https://www.youtube.com/watch?v=gzivWXNW8Jc
 }
 
 export function buscarSuperheroesPorAtributoController(req, res) {
   const { atributo, valor } = req.params
   const superheroes = buscarSuperheroesPorAtributo(atributo, valor)
-  if (superheroes) {
-    res.send(renderizarListaSuperheroes(superheroes))
-  } else {
+  if (!superheroes.length)
     res.status(404).send({mensaje: 'No se encontraron superhéroes con ese atributo'})
-  }
+
+  res.status(200).send(renderizarListaSuperheroes(superheroes))
 }
 
 export function obtenerSuperheroesMayoresDe30Controller(req, res) {
   const superheroes = obtenerSuperheroesMayoresDe30()
-  res.send(renderizarListaSuperheroes(superheroes))
+  if(!superheroes.length)
+    res.status(404).send({mensaje: 'No se encontraron superhéroes que cumplan con las condiciones establecidas'})
+  
+  res.status(200).send(renderizarListaSuperheroes(superheroes))
 }
